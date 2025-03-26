f859b7e0988d2a1688912e372cdd3cacd0c70714
eduardoreyes007351208 <lalosoccer307@gmail.com>
2025-03-26 16:45:28 -0700

    feat: Create function to generate grid squares

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 /* Create the squares that go inside the main container */
+const gridContainer = document.querySelector('#gridContainer')
+
+let createSquares = () => {
+    
+    let i = 0;
+    while (i < 16) {
+        let gridSquare = document.createElement('div')
+        gridSquare.setAttribute('style', 'width: 10px; height: 10px; border: 1px solid black')
+        gridContainer.appendChild(gridSquare)
+        console.log(i)
+        i++
+    }
+}
+
+
+createSquares()
