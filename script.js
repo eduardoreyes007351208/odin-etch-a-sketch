1736ba87a40f0e03645d2a6eaedc665482632371
eduardoreyes007351208 <lalosoccer307@gmail.com>
2025-03-27 11:23:13 -0700

    feat: Create btn and func to create custom size
    Create a function and button that allows the user
    to create a new canvas between 16 squares wide to
    100 squares wide.

--- script.js
+++ script.js
@@ -2,38 +2,72 @@
 let colorHex = 'black'
 const gridContainer = document.querySelector('#gridContainer')
 const colorBtn = document.querySelector('#colorBtn')
+const sizeBtn = document.querySelector('#sizeBtn')
 
 
 colorBtn.addEventListener('click', () => {
     chooseColor()
 })
+sizeBtn.addEventListener('click', () => {
+    customSize()
+})
+
 let chooseColor = () => {
     colorHex = prompt('Type in the color hex code or color name')
-    console.log(colorHex)
 }
-let createSquares = () => {
-    
+let createSquares = (size) => {
+    let squareWidth = 0
+
+    switch (true) {
+        case size >= 16 && size <= 25:
+            squareWidth = 30
+            break
+        case size > 25 && size <= 50:
+            squareWidth = 20
+            break
+        case size > 50 && size <= 100:
+            squareWidth = 10
+            break
+        default:
+            squareWidth = 10
+    }
+
+
+    gridContainer.setAttribute('style', `width: ${(size*squareWidth)+2}px; height: ${(size*squareWidth)+2}px`)
     let i = 0;
-    while (i < (16*16)) {
+    while (i < (size * size)) {
         let gridSquare = document.createElement('div')
         gridSquare.classList.add('squares')
-        gridSquare.setAttribute('style', 'width: 40px; height: 40px; flex-basis: 40px; border: 1px solid black')
+        gridSquare.setAttribute('style', `width: ${squareWidth}px; height: ${squareWidth}px; flex-basis: ${squareWidth}px; border: 1px solid black`)
         gridContainer.appendChild(gridSquare)
         i++
     }
 }
-
-createSquares()
-const grids = document.querySelectorAll('.squares')
-
-grids.forEach((square) => {
-    
-    square.addEventListener('mouseover', () => {
-        if (colorHex == '' || colorHex == null) {
-            square.style.background = 'black'
-        } else {
-            square.style.background = `${colorHex}`
-        }
-        
+let customSize = () => {
+    let size = prompt('Choose the size for the grid (Max Size 100)')
+    const grids = document.querySelectorAll('.squares')
+    grids.forEach((square) => {
+        gridContainer.removeChild(square)
     })
-})
+    createSquares(size)
+    draw()
+}
+
+let draw = () => {
+    const grids = document.querySelectorAll('.squares')
+
+    grids.forEach((square) => {
+    
+        square.addEventListener('mouseover', () => {
+            if (colorHex == '' || colorHex == null) {
+                square.style.background = 'black'
+            } else {
+                square.style.background = `${colorHex}`
+            }
+        
+        })
+    })
+}
+
+createSquares(16)
+draw()
