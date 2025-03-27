ee390d3b1ac1489920f01e84aac25fdadd3bb540
eduardoreyes007351208 <lalosoccer307@gmail.com>
2025-03-26 17:36:09 -0700

    fix: Fix the sizing and flex wrap

--- script.js
+++ script.js
@@ -4,9 +4,9 @@ const gridContainer = document.querySelector('#gridContainer')
 let createSquares = () => {
     
     let i = 0;
-    while (i < 16) {
+    while (i < (16)) {
         let gridSquare = document.createElement('div')
-        gridSquare.setAttribute('style', 'width: 10px; height: 10px; border: 1px solid black')
+        gridSquare.setAttribute('style', 'width: 10px; height: 10px; border: 1px solid black; flex-basis: 10px')
         gridContainer.appendChild(gridSquare)
         console.log(i)
         i++
